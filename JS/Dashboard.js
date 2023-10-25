0033d2a42431c6969af53f70e776e07d448a9238
LuqAndrade <lucasdz2021@gmail.com>
2023-10-25 13:05:34 -0300

    fazendo o grafico

--- JS/Dashboard.js
+++ JS/Dashboard.js
@@ -1,21 +1,23 @@
-const ctx = document.getElementById('topLivros');
 
-new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-      label: 'Top 6 Livros Mais Alugados',
-      data: [12, 19, 3, 5, 2, 3], //quantidade de livros
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
+  const ctx = document.getElementById('topLivros');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
       }
     }
-  }
-});
+  });
+
 
